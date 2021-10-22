53b3badc1f323e27604307600a7ee58c8c82f0aa
naxomi <anjou.raphael@gmail.com>
2021-10-22 10:45:27 +0200

    Add method for question3_4 in TestFichier.java file

--- java_td5/TestFichier.java
+++ java_td5/TestFichier.java
@@ -1,8 +1,19 @@
 package java_td5;
 
 public class TestFichier {
-    public static void main(String[] args) {
+
+    public static void question3_4() {
         Fichier fichier = new Fichier("fileQ3");
         fichier.aleatoire(100);
+
+        /*
+         * Answer to question 4 : we can see that the file size is equal to 100*4 = 400
+         * octets. Which mean that we have exactly created 100 random numbers, each with
+         * 4 octets.
+         */
+    }
+
+    public static void main(String[] args) {
+        question3_4();
     }
 }
